28f4a1844269fb0194c4b0000d54d14f103648d3
courtney-swift <courtneydezman@gmail.com>
2026-06-26 09:30:09 +0900

    First Update

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import "./components/Navbar"
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+function App() {
+
+  return (
+    <div>
+    <h1>Hello From React!!</h1>
+    <Navbar/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-store/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import "./Navbar.css"
+
+function Navbar(){
+    return(
+        <h1>Welcome to my online Store</h1>
+    )
+};
+
+export default Navbar;
